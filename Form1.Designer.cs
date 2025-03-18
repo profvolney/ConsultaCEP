7722ad630d2f1508db301b7722dd2af6042c6142
jason.vsilva <jason.vsilva@JBQ0684377W10-1>
2025-03-18 17:29:09 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ConsultaCEP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            btn_buscar = new Button();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(73, 116);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(353, 43);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(73, 227);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(353, 35);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(73, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 32);
+            label1.TabIndex = 2;
+            label1.Text = "CEP";
+            // 
+            // btn_buscar
+            // 
+            btn_buscar.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_buscar.Location = new Point(512, 116);
+            btn_buscar.Name = "btn_buscar";
+            btn_buscar.Size = new Size(178, 43);
+            btn_buscar.TabIndex = 3;
+            btn_buscar.Text = "Buscar";
+            btn_buscar.UseVisualStyleBackColor = true;
+            btn_buscar.Click +=  btn_buscar_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(73, 337);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(353, 35);
+            textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(73, 194);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 30);
+            label2.TabIndex = 5;
+            label2.Text = "Logradouro";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(73, 304);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 30);
+            label3.TabIndex = 6;
+            label3.Text = "Bairro";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(512, 227);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(178, 35);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(512, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 30);
+            label4.TabIndex = 8;
+            label4.Text = "Cidade";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(512, 342);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(178, 35);
+            textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(512, 309);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 30);
+            label5.TabIndex = 10;
+            label5.Text = " UF";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(306, 21);
+            label6.Name = "label6";
+            label6.Size = new Size(215, 37);
+            label6.TabIndex = 11;
+            label6.Text = "Buscador de CEP";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(btn_buscar);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Button btn_buscar;
+        private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace ConsultaCEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
